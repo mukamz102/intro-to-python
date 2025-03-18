780f5698862d2c85487198d4b355a1ce806ae651
Ritchelle <graceritchelle@gmail.com>
2025-03-18 03:23:42 -0400

    python basics

---
+++ conditions.py
@@ -0,0 +1,53 @@
+#conditional statements
+x = 10
+y = 50
+print(x)
+#snake casing
+my_city="nairobi"
+#camel casing
+myOldestCar ="Suzuki"
+
+ name = "mukami"
+ #types of data
+ #numbers
+ height=3
+ weight = 60.45
+ #strings
+ full_name = "Jack Kim"
+ distance = "45"
+ #boolean
+ is_listed = True
+ is_disabled = False
+ #conditional statements
+ password = "1234456"
+ db_password = "654321"
+
+ if password==db_password:
+     print("Success")
+else
+     print("Failed")
+score = 64
+if score>=90:
+    print("A")
+elif score>=80 and score <=89:
+    print("A-")
+elif score>=75 and score <=79:
+    print("B+")
+elif score>=70 and score <=74:
+    print("B")
+elif score>=65 and score <=69:
+    print("B-")
+elif score>=60 and score <=64:
+    print("C+")
+elif score>=55 and score <=59:
+    print("C")
+elif score>=50 and score <=54:
+    print("C-")
+elif score>=45 and score <=49:
+    print("D+")
+elif score>=40 and score <=44:
+    print("D")
+elif score>=35 and score <=39:
+    print("D-")
+else:
+    print("E")
---
+++ strings.py
@@ -0,0 +1,12 @@
+message = "hello how are you doing?"
+print(message)
+#functions
+print(message.capitalize())
+print(message.lower())
+print(message.upper())
+print(message.islower())
+print(message.isalpha())
+print(message.count("are"))
+print(message.title())
+print(message.replace("doing ","going"))
+print(message)
